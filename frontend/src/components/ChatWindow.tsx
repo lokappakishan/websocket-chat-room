2ddc4b41a42feff37457626cac559314fcf1eeee
kishan lokappa <lokappakishan@gmail.com>
2025-04-23 10:02:09 +0200

    feat: set up static chat UI to scaffold room based chat interface

---
+++ frontend/src/components/ChatWindow.tsx
@@ -0,0 +1,49 @@
+const ChatWindow = () => {
+  return (
+    <div className="h-[80%]">
+      <h2 className="p-4 font-[400] text-4xl border-b border-gray-200">
+        General
+      </h2>
+      <div>
+        <div className="chat chat-start">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-gray-100 text-gray-600">
+            Hey there, welcome!
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-blue-50 text-gray-600">
+            Thanks, happy to join!
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-gray-100 text-gray-600">
+            Let me know if you need anything.
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-gray-100 text-gray-600">
+            Hey there, welcome!
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-blue-50 text-gray-600">
+            Thanks, happy to join!
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-header">Obi-Wan Kenobi</div>
+          <div className="chat-bubble bg-gray-100 text-gray-600">
+            Let me know if you need anything.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatWindow;
